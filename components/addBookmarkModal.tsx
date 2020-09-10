c90fcb2b92d7b4904b1b92b3138bc920482cabbb
tomi <bkc.tomi1979@gmail.com>
2020-09-10 16:48:06 +0900

    change addBookmarkModal.tsx

--- components/addBookmarkModal.tsx
+++ components/addBookmarkModal.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Styles from "../styles/AddBookmarkModal.module.css";
+import Styles from "../styles/addBookmarkModal.module.css";
 
 export default function AddBookmarkModal() {
     const [modalCls, setModalCls] = useState(Styles.hide);
