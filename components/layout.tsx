587403a178fb560ab5b33bc6d598edf059bade2f
tomi <bkc.tomi1979@gmail.com>
2020-09-14 19:26:17 +0900

    index change

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,7 +4,8 @@ import Header from "./header";
  * header, firebase objectを読み込ませる予定
  * 全てのpageをこのコンポーネントでラップする。
  */
-export const SiteTitle = "SIORIN - Bookmark Manager -"
+export const SiteTitle = "SIORIN - Bookmark Manager -";
+export const Description = "このサイトはブックマークを管理するサイトです。ブックマークを大量に登録しても、タグ検索で簡単に検索できます。また他のユーザーが公開しているブックマークも検索することができます。";
 
 export default function Layout({ children }:{ children: React.ReactNode }) {
     
@@ -15,9 +16,13 @@ export default function Layout({ children }:{ children: React.ReactNode }) {
                 <link rel="icon" href="/favicon.ico" />
                 <meta
                     name="description"
-                    content="this is bookmark manager with tag searching."
+                    content={ Description }
                 />
-                
+                <meta name="theme-color" content="#16776f" />
+                <meta property="og:title" content={ SiteTitle }></meta>
+                <meta property="og:url" content="https://siorin.vercel.app/"></meta>
+                <meta property="og:description" content={ SiteTitle }></meta>
+                <meta property="og:image" content="https://siorin.vercel.app/static/siori.svg"></meta>
             </Head>
             <Header />
             <main>
